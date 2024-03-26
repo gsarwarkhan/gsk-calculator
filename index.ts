4738e24781c1256cc557928052b0e17ebc7e3e3d
Ghulam Sarwar Khane <sarwaronline@gmail.comm>
2024-03-27 04:28:24 +0500

    Adding gsk calculator

---
+++ index.ts
@@ -0,0 +1,39 @@
+#! /usr/bin/env node
+
+import inquirer from "inquirer";
+import Choices from "inquirer/lib/objects/choices.js";
+
+// Printing of Welcome Message
+console.log("\n\tWelcome to \'Ghulam Sarwar Khan'\ CLI Calculator");
+
+// Asking question from user through inquirer
+
+const answers = await inquirer.prompt([
+    {message: "Enter First Number", type: "number", name: "firstNumber"},
+    {message: "Enter Second Number", type: "number", name: "secondNumber"},
+
+    {
+        message: "Select one Operator to perform Operation",
+        type: "list",
+        name: "operator",
+        choices: ["Addition", "Subtraction", "Multiplication", "Division"],
+    },
+]);
+
+// conditional statement if - Else
+
+if(answers.operator === "Addition"){
+    console.log(answers.firstNumber + answers.secondNumber)
+}
+else if (answers.operator === "Subtraction"){
+    console.log(answers.firstNumber - answers.secondNumber)
+}
+else if (answers.operator === "Multiplication"){
+    console.log(answers.firstNumber * answers.secondNumber)
+}
+else if (answers.operator === "Division"){
+    console.log(answers.firstNumber / answers.secondNumber)
+}
+else{
+    console.log("Invalid Input")
+}
